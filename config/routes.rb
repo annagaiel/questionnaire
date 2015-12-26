c8dde26da25d0953acb00050e7fa9f6f79fb65fe
annagaiel <annagaiel@gmail.com>
2015-12-26 10:03:06 -0800

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root 'survey#index'
+  resources :surveys
+  root to: 'surveys#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
