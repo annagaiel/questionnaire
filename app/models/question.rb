7b04ce8e77c866f1e75afc9419a0e021f20516bc
annagaiel <annagaiel@gmail.com>
2015-12-26 18:09:50 -0800

    Adding survey

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ActiveRecord::Base
+  # attr_accessible :content, :survey_id
+  belongs_to :survey
+  has_many :answers
+  accepts_nested_attributes_for :answers, allow_destroy: true
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,2 +1,5 @@
 class Survey < ActiveRecord::Base
+  # attr_accessible :name
+  has_many :questions
+  accepts_nested_attributes_for :questions, allow_destroy: true
 end
